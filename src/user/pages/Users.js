6358821ae852c6f5b0cf93cc04c5338473acf840
Ian Connell <ian10gt@gmail.com>
2023-08-27 12:33:34 +0100

    lesson 54: Adding a Modal

---
+++ src/user/pages/Users.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Scwhartz",
+      image: "https://jpeg.org/images/jpeg-home.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
